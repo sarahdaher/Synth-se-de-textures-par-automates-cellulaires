3366e095fa4ea6a13e2e01d92eaf9ac4decb5ed6
Baptiste LEFEBVRE <baptiste.lefebvre@telecom-paris.fr>
2026-06-16 22:26:38 +0200

    Merge branch

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ MULTI_TEX = False
 
 
 if not MULTI_TEX:
-    IMAGE_PATH = "textures/dotted.jpg"
+    IMAGE_PATH = "textures/bubbles.png"
     C = 12
 
 
@@ -42,7 +42,7 @@ if MULTI_TEX:
 ###############################################################################################
 
 OUT_DIR = "output"
-STEPS = 20000
+STEPS = 10000
 BATCH = 4
 SIZE = 128
 HIDDEN = 96
@@ -52,9 +52,9 @@ LAYERS = [1, 6, 11, 18]
 MEAN   = [0.485, 0.456, 0.406] #moyenne imagenet
 STD    = [0.229, 0.224, 0.225] #std imagenet
 
-PRESET = 0
+PRESET = 1
 
-LOSS = "sot"  # sot ou gram
+LOSS = "gram"  # sot ou gram
 
 INFERENCE = False
 # !!!! si multi-textures :
@@ -63,7 +63,7 @@ INFERENCE = False
 NB_IMGS = 10
 
 # mac
-device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+#device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
 
 #pas mac
-#device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
